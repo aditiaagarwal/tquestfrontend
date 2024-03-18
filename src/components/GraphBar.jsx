3e753b9810229109ba0b025fd5913a075db8c0cd
RajGupta15082004 <graj25776@gmail.com>
2024-03-18 14:25:54 +0530

    frontend done

--- src/components/GraphBar.jsx
+++ src/components/GraphBar.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import MyChart from './MyChart.jsx';
 import './GraphBar.css';
 
-const GraphBar = () => {
+const GraphBar = ({}) => {
   // Sample data for the charts
   const chartData1 = {
     labels: ['Lower Bound', 'Value', 'Upper Bound'],
@@ -12,37 +12,7 @@ const GraphBar = () => {
     upperBound: 16, // Adjust upper bound as needed
   };
 
-  // const chartData2 = {
-  //   labels: ['Lower Bound', 'Value', 'Upper Bound'],
-  //   datasetLabel: 'Test Result',
-  //   data: [12, 25, 16], // Value exceeds upper bound
-  //   lowerBound: 12, // Adjust lower bound as needed
-  //   upperBound: 16, // Adjust upper bound as needed
-  // };
-
-  // const chartData3 = {
-  //   labels: ['Lower Bound', 'Value', 'Upper Bound'],
-  //   datasetLabel: 'Test Result',
-  //   data: [12, 19, 25], // Value exceeds upper bound
-  //   lowerBound: 12, // Adjust lower bound as needed
-  //   upperBound: 25, // Adjust upper bound as needed
-  // };
-
-  // const chartData4 = {
-  //   labels: ['Lower Bound', 'Value', 'Upper Bound'],
-  //   datasetLabel: 'Test Result',
-  //   data: [15, 19, 29], // Value exceeds upper bound
-  //   lowerBound: 15, // Adjust lower bound as needed
-  //   upperBound: 29, // Adjust upper bound as needed
-  // };
-
-  // const chartData5 = {
-  //   labels: ['Lower Bound', 'Value', 'Upper Bound'],
-  //   datasetLabel: 'Test Result',
-  //   data: [12, 19, 13], // Value exceeds upper bound
-  //   lowerBound: 12, // Adjust lower bound as needed
-  //   upperBound: 13, // Adjust upper bound as needed
-  // };
+  
 
   return (
     <div className="container">
--- src/components/visualization.jsx
+++ src/components/visualization.jsx
@@ -1,35 +1,53 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from "react";
 import Navbar from "./Navbar";
-import axios from 'axios';
-import GraphBar from "./GraphBar.jsx";
-import './visualization.css';
-import {
-  MDBCard,
-  MDBCardBody,
-  MDBCardTitle,
-} from 'mdb-react-ui-kit';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import axios from "axios";
+import { Transition } from "react-transition-group"; // Import Transition
+import { MDBCard, MDBCardBody, MDBCardTitle } from "mdb-react-ui-kit";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import DialogActions from "@mui/material/DialogActions";
+import DialogContent from "@mui/material/DialogContent";
+import DialogContentText from "@mui/material/DialogContentText";
+import DialogTitle from "@mui/material/DialogTitle";
 import Link from "./Link";
-const BarCharts = ({}) => {
-  const bookingId = localStorage.getItem('bookingId');
+import { BarChart } from "@mui/x-charts/BarChart";
+
+import "./visualization.css";
+
+const BarCharts = () => {
+  const [open, setOpen] = React.useState(false);
+
+  const handleClickOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+  const bookingId = localStorage.getItem("bookingId");
   const [data, setData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [jokeResponses, setJokeResponses] = useState({});
+  const [firstEffectCompleted, setFirstEffectCompleted] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await axios.get(`http://10.81.73.1:3000/api/bookings?booking_id=${bookingId}`);
-        const parsedData = response.data.map(item => ({
+        const response = await axios.get(
+          `http://10.81.73.1:3000/api/bookings?booking_id=${bookingId}`
+        );
+        const parsedData = response.data.map((item) => ({
           ...item,
-          test_values: JSON.parse(item.test_values)
+          test_values: JSON.parse(item.test_values),
         }));
         setData(parsedData);
         setLoading(false);
+        setFirstEffectCompleted(true); // Set flag to true after completing the API call
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
       }
     };
 
@@ -39,14 +57,27 @@ const BarCharts = ({}) => {
   const processValues = () => {
     const processedData = {};
     for (let i = 0; i < data.length; i++) {
-      const test_name = data[i]['test_name'];
-      for (let j = 0; j < data[i]['test_values'].length; j++) {
-        const parameter_value = data[i]['test_values'][j]['parameter_name'];
-        const parameterValue = parseFloat(data[i]['test_values'][j]['parameter_value'], 10);
-        const lowerBound = parseFloat(data[i]['test_values'][j]['lower_bound'], 10);
-        const upperBound = parseFloat(data[i]['test_values'][j]['upper_bound'], 10);
+      const test_name = data[i]["test_name"];
+      for (let j = 0; j < data[i]["test_values"].length; j++) {
+        const parameter_value = data[i]["test_values"][j]["parameter_name"];
+        const parameterValue = parseFloat(
+          data[i]["test_values"][j]["parameter_value"],
+          10
+        );
+        const lowerBound = parseFloat(
+          data[i]["test_values"][j]["lower_bound"],
+          10
+        );
+        const upperBound = parseFloat(
+          data[i]["test_values"][j]["upper_bound"],
+          10
+        );
 
-        if (!isNaN(parameterValue) && !isNaN(lowerBound) && !isNaN(upperBound)) {
+        if (
+          !isNaN(parameterValue) &&
+          !isNaN(lowerBound) &&
+          !isNaN(upperBound)
+        ) {
           if (!processedData[test_name]) {
             processedData[test_name] = [];
           }
@@ -62,31 +93,113 @@ const BarCharts = ({}) => {
     return processedData;
   };
   const processedData = processValues();
-                      
-  
+  if (loading) {
+    return <div>Loading...</div>;
+  }
 
   return (
     <>
       <Navbar />
       <Link />
       {Object.keys(processedData).map((testName, index) => (
-        <div key={index} style={{ marginTop: '40px' }}>
-          <MDBCard style={{ backgroundColor: '#B9D9EB' }}>
+        <div key={index} style={{ marginTop: "40px" }}>
+          <MDBCard style={{ backgroundColor: "#B9D9EB" }}>
             <MDBCardBody>
-              <MDBCardTitle style={{ fontWeight: 'bold', color: 'blue', fontSize: '24px', textAlign: 'center' }}>{testName}</MDBCardTitle>
+              <MDBCardTitle
+                style={{
+                  fontWeight: "bold",
+                  color: "blue",
+                  fontSize: "24px",
+                  textAlign: "center",
+                }}
+              >
+                {testName}
+              </MDBCardTitle>
               <ul>
                 {processedData[testName].map((item, idx) => (
                   <li key={idx}>
                     <br />
-                    <Card sx={{ maxWidth: 1000, margin: 'auto', textAlign: 'center', background: 'white' }}>
-                      <CardContent>
-                        <Typography gutterBottom variant="h5" component="div" style={{ color: 'black', fontWeight: 'bold', fontFamily: 'Arial', fontSize: '40' }}>
-                          {item.parameter_value}
-                        </Typography>
-
-                        <div className="graph-center">
-                        <GraphBar/>
+                    <Card
+                      sx={{
+                        maxWidth: 1000,
+                        margin: "auto",
+                        textAlign: "center",
+                        background: "white",
+                      }}
+                    >
+                      <CardContent
+                        style={{
+                          display: "flex",
+                          justifyContent: "space-between",
+                          alignItems: "center",
+                        }}
+                      >
+                        <div>
+                          <Typography
+                            gutterBottom
+                            variant="h5"
+                            component="div"
+                            style={{
+                              color: "black",
+                              fontWeight: "bold",
+                              fontFamily: "Arial",
+                              fontSize: "40",
+                            }}
+                          >
+                            {item.parameter_value}
+                          </Typography>
+                          <BarChart
+                            xAxis={[
+                              {
+                                scaleType: "band",
+                                data: [item.parameter_value],
+                              },
+                            ]}
+                            series={[
+                              { data: [item.lowerBound] },
+                              { data: [item.parameterValue] },
+                              { data: [item.upperBound] },
+                            ]}
+                            width={500}
+                            height={300}
+                          />
                         </div>
+                        <Button
+                          variant="outlined"
+                          onClick={handleClickOpen}
+                          style={{
+                            backgroundColor: "#7CB9E8",
+                            width: "300px",
+                            color: "black",
+                          }}
+                        >
+                          INFER RESULTS
+                        </Button>
+                        <Dialog
+                          open={open}
+                          onClose={handleClose}
+                          aria-labelledby="alert-dialog-title"
+                          aria-describedby="alert-dialog-description"
+                        >
+                          <DialogTitle id="alert-dialog-title">
+                            {"THIS IS WHAT YOUR GRAPH SHOWS"}
+                          </DialogTitle>
+                          <DialogContent>
+                            <DialogContentText id="alert-dialog-description">
+                              {item.parameterValue < item.lowerBound
+                                ? "Your value is less than the desired value. Please see Smart Interpretation for outcome."
+                                : item.parameterValue > item.upperBound
+                                ? "Your value is higher than the desired value. Please refer to Smart Interpretation."
+                                : "Your value is within the desired range."}
+                            </DialogContentText>
+                          </DialogContent>
+                          <DialogActions>
+                            <Button onClick={handleClose}>Disagree</Button>
+                            <Button onClick={handleClose} autoFocus>
+                              Agree
+                            </Button>
+                          </DialogActions>
+                        </Dialog>
                       </CardContent>
                     </Card>
                   </li>
